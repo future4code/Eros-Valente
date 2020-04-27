9096837e9b42ff8f3c69b7ef9337263f0583b759
Eros Pinder Valente <eros.pv@gmail.com>
2020-04-27 16:55:04 -0300

    exercicios de js aula9

---
+++ semana3/logica-booleana/js-vanilla-template/src/index.js
@@ -0,0 +1,50 @@
+/* EXERCÍCIOS DE INTERPRETAÇÃO
+
+exercício 1
+
+a. false
+b. false
+c. true
+d. false
+e. boolean
+
+exercício 2
+
+a. variaveis que guardam mais de uma informação ao mesmo tempo;
+   se declara colocando o conteudo de sua variável entre colchetes e separados por virgulas;
+   ex: let array = [1, 2, 3, 4] 
+
+b. 0
+
+c. usando o comando .length;
+ex: array.length
+
+d. I. undefined
+   II. null
+   III. 11
+   IV. 3 e 4
+   V. 19 e 9
+   VI. 3
+   VII. 1
+
+------------------------------------------------------- */
+
+
+// EXERÍCICIOS DE ESCRITA DE CÓDIGO//
+
+let F = 77;
+let K = (((F - 32) * 5) / 9) + 273.15;
+console.log ("ex 1.a- ", K, "K");
+
+let C = 80;
+F = ((C * 9) / 5) + 32;
+console.log ("ex 1.b-", F, "°F")
+
+C = 30;
+F = ((C * 9) / 5) + 32;
+K = (((F - 32) * 5) / 9) + 273.15
+console.log ("ex 1.c-", F, "°F e", K, "K")
+
+
+
+
