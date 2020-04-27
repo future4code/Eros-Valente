70b9749a8f1cec11f006bb14c7ddab2d73450e57
Eros Pinder Valente <eros.pv@gmail.com>
2020-04-27 18:46:38 -0300

    exercicios de js aula 9 completos mais desafios

--- semana3/logica-booleana/js-vanilla-template/src/index.js
+++ semana3/logica-booleana/js-vanilla-template/src/index.js
@@ -30,21 +30,114 @@ d. I. undefined
 ------------------------------------------------------- */
 
 
-// EXERÍCICIOS DE ESCRITA DE CÓDIGO//
+// EXERÍCICIOS DE ESCRITA DE CÓDIGO
 
-let F = 77;
-let K = (((F - 32) * 5) / 9) + 273.15;
-console.log ("ex 1.a- ", K, "K");
+// Exercício 1
 
-let C = 80;
-F = ((C * 9) / 5) + 32;
-console.log ("ex 1.b-", F, "°F")
+console.log("EXERCÍCIOS DE ESCRITA DE CÓDIGO");
+console.log("");
+console.log("EXERCÍCIO 1");
 
-C = 30;
-F = ((C * 9) / 5) + 32;
-K = (((F - 32) * 5) / 9) + 273.15
-console.log ("ex 1.c-", F, "°F e", K, "K")
+let f = 77;
+let k = (((f - 32) * 5) / 9) + 273.15;
+console.log ("a.", k, "K");
+
+c = 80;
+f = ((c * 9) / 5) + 32;
+console.log ("b.", f, "°F")
+
+c = 30;
+f = ((c * 9) / 5) + 32;
+k = (((f - 32) * 5) / 9) + 273.15;
+console.log ("c.", f, "°F e", k, "K");
+
+c = prompt("Digite uma temperatura em Celsius para convertê-la");
+f = ((c * 9) / 5) + 32;
+k = (((f - 32) * 5) / 9) + 273.15;
+console.log ("d.", f, "°F e", k, "K"); 
+
+// Exercício 2
 
 
+let nome = prompt("Qual o seu nome?")
+let idade = prompt("Qual a sua idade?")
+let filme = prompt("Você tem algum filme favorito?")
+let viagem = prompt("Qual país no mundo você mais tem vontade de conhecer?")
+let animal = prompt("Que animal você gostaria de ser?")
+
+console.log("")
+console.log("EXERCÍCIO 2");
+console.log("1. Qual o seu nome?")
+console.log("Resposta:", nome)
+console.log("Qual sua idade?")
+console.log("Resposta:", idade)
+console.log("Você tem algum filme favorito?")
+console.log("Resposta:", filme)
+console.log("Qual país no mundo você mais tem vontade de conhecer?")
+console.log("Resposta:", viagem)
+console.log("Que animal você gostaria de ser?")
+console.log("Resposta:", animal)
+
+// Exercício 3
+
+console.log("");
+console.log("EXERCÍCIO 3");
+let kwhValor = 0.05;
+let kwhQuantidade = 280;
+let valorTotalKwh = kwhQuantidade * kwhValor
+console.log("a.", valorTotalKwh);
+
+let desconto = 0.15;
+valorDesconto = valorTotalKwh * desconto;
+valorTotalKwh -= valorDesconto
+
+console.log("b.", valorTotalKwh)
+
+// Desafios
+
+console.log("")
+console.log("DESAFIOS")
+
+// item a.
+
+let libra = 20
+let kg = libra / 2.20462
+console.log("a. 20lb equivalem a", kg, "kg")
 
 
+// item b.
+
+let onca = 10.5
+kg = onca / 35.274  
+console.log("b. 10.5oz equivalem a", kg, "kg")
+
+// item c.
+
+let milhas = 100
+let metros = milhas / 0.000621371
+console.log("c. 100mi equivalem a", metros, "m")
+
+// item d
+
+let feets = 50
+metros = feets / 3.28084
+console.log("d. 50ft equivalem a", metros, "m")
+
+// item e
+
+let galao = 103.56
+let litros = galao / 0.264172
+console.log("e. 103.56gal equivalem a", litros, "l")
+
+// item f
+
+let xicaras = 450
+litros = xicaras / 4.2268
+console.log("f. 450xic equivale a", litros, "l")
+
+// item g
+// modificando item d
+
+feets = prompt("Quntos pés deseja converter em metros?")
+metros = feets / 3.28084
+console.log("g. ", feets + "ft equivalem a", metros, "m")
