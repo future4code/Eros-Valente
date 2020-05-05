841d4252a1217bb50c75341b49ea2cc9aec68e76
Eros Pinder Valente <eros.pv@gmail.com>
2020-05-05 16:53:07 -0300

    planner incompleto

---
+++ semana4/planner/index.js
@@ -0,0 +1,9 @@
+const tarefaAdicionada = document.getElementById("novaTarefa")
+const dia = document.getElementById("diaDaSemana")
+
+function adicionarTarefa() {
+    let tarefa = tarefaAdicionada.value
+    console.log(tarefa) // APAGAR ANTES DE FINALIZAR
+    let diaEscolhido = dia.value
+
+}
