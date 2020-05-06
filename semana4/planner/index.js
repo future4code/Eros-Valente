d3f2c5b1ea3b39ca07bc75c2ab02a32e75b5029c
Eros Pinder Valente <eros.pv@gmail.com>
2020-05-06 11:46:59 -0300

    inclusão do desafio do botão para apagar itens

--- semana4/planner/index.js
+++ semana4/planner/index.js
@@ -1,6 +1,13 @@
 const tarefaAdicionada = document.getElementById("novaTarefa")
 const diaSeletor = document.getElementById("diaDaSemana")
 
+function apertaEnter(evento){
+    console.log(evento.key)
+    if(evento.key === "Enter") {
+        adicionarTarefa()
+    }
+}
+
 function adicionarTarefa() {
     let tarefa = tarefaAdicionada.value
     if (tarefa === "") {
@@ -13,5 +20,16 @@ function adicionarTarefa() {
     tarefaAdicionada.value = ""
 }
 
+function limparTarefas() {  
+  let divs = document.getElementsByTagName("ul")
+    for (let i = 0; i < divs.length; i++) {
+       let divAcessada = document.getElementsByTagName("ul")[i]
+        let itenslistas = divAcessada.getElementsByTagName("li")
+        for (let j = 0 ; itenslistas.length > 0; j++) {
+            itenslistas[itenslistas.length - 1].remove()
+        }    
+    }
 
+    
+}
 
