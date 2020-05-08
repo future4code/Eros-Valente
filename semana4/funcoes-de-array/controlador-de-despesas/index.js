00fce4d0c946d168e20065e49bfc3bc268bc208c
Eros Pinder Valente <eros.pv@gmail.com>
2020-05-08 11:37:05 -0300

    exercício função de arrays

--- semana4/funcoes-de-array/controlador-de-despesas/index.js
+++ semana4/funcoes-de-array/controlador-de-despesas/index.js
@@ -7,23 +7,38 @@ const infoDespesa = {
 
 let despesas = []
 
-function cadastraDespesa(){
-    event.preventDefault()
+function cadastraDespesa() {
+    
     let valorDespesa = document.getElementById("valor")
     let tipoDespesa = document.getElementById("seletor-tipo")
     let descricaoDespesa = document.getElementById("descricao")
-    
     let novaDespesa = {
         ...infoDespesa,
         valor: Number(valorDespesa.value),
         tipo: tipoDespesa.value,
         descricao: descricaoDespesa.value
     }
+    if (novaDespesa.valor === 0 || novaDespesa.tipo === "" || novaDespesa.descricao === "") {
+        alert("Preencha todos os campos.")
+    } else {
+        despesas.push(novaDespesa)
+        valorDespesa.value = tipoDespesa.value = descricaoDespesa.value = ""
+        console.log(despesas)   
+    }
+    despesas.forEach((gasto, idx, arr) => {
+        document.getElementById("lista-despesas").innerHTML += `<li>R$${gasto[arr.length - 1].valor} - ${gasto[arr.length - 1].tipo} - ${gasto[arr.length - 1].descricao}</li>`
+    })
     
-    despesas.push(novaDespesa)
-    valorDespesa.value = tipoDespesa.value = descricaoDespesa.value = ""
-    console.log(despesas)
-    console.log(typeof(novaDespesa.valor))
-    console.log(tipoDespesa)
 
-}
+
+}   
+
+
+
+// console.log(despesasTotais)
+
+// function filtraDespesas() {
+//     despesas = cadastraDespesa()  
+//     console.log(desepesas)
+
+// }
