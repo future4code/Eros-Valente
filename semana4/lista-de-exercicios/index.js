1cf4704cda1290fbb857c05523a6be1bf886dde3
Eros Pinder Valente <eros.pv@gmail.com>
2020-05-08 17:09:43 -0300

    exercício 1 de interpretação de código

---
+++ semana4/lista-de-exercicios/index.js
@@ -0,0 +1,19 @@
+/*
+
+LISTA DE EXERCÍCIOS 
+
+EXERCÍCIOS DE INTERPRETAÇÃO DE CÓDIGO
+
+EXERCÍCIO 1
+
+
+
+Objetivo principal da função: A função conversorDeMoeda irá converter o valor recebido em reais (R$) para dólar
+
+O que acontece dentro da função: A variável cotação irá receber o valor digitado pelo usuário no prompt que sera a cotação do Dólar, o valor é convertido de string para número (todo valor recebido através do prompt entra nó código como string).
+a linha seguinte é o return da função que retornará a string "R$" seguida do valor recebido pela função multiplicado pelo valor da variável cotação (que foi digitada no prompt)
+
+fora da função a variável meuDinheiro chama a função conversorDeMoedas com o valor que se deseja converter
+
+o que é impresso no console: no console será impresso o valor da constante meuDinheiro
+
