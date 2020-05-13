548c40f2d16cee0637f6b8d50cfe5c492fff9681
Eros Pinder Valente <eros.pv@gmail.com>
2020-05-13 16:56:46 -0300

    exercicio componentes de classe labegram semana5-aula19

---
+++ semana5/labedin/build/precache-manifest.8e2b2a8f79474d3929111a8d18af47b9.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d2d0053ad4b7e5255f347791d7afe844",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1f37e602bece2ccfa4f2",
+    "url": "/static/css/main.4fb82960.chunk.css"
+  },
+  {
+    "revision": "2735e5d6fe7da1972309",
+    "url": "/static/js/2.b639868a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.b639868a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f37e602bece2ccfa4f2",
+    "url": "/static/js/main.2f83962f.chunk.js"
+  },
+  {
+    "revision": "9cec4286d4e96efb4903",
+    "url": "/static/js/runtime-main.6b07746c.js"
+  },
+  {
+    "revision": "3a8e847f09957227d61ef677120e0ea9",
+    "url": "/static/media/email.3a8e847f.svg"
+  },
+  {
+    "revision": "4654fcf5c467cba85d1207b13ab7489d",
+    "url": "/static/media/location.4654fcf5.svg"
+  },
+  {
+    "revision": "a147009b38eb9b1244577b04d033db6f",
+    "url": "/static/media/minha-foto.a147009b.jpg"
+  }
+]);
