f6f6153681973ae4c488b6583d1b7ae5b78aa7cd
Eros Pinder Valente <eros.pv@gmail.com>
2020-05-24 14:09:26 -0300

    pasta build adicionada

---
+++ semana6/lista-tarefas/build/precache-manifest.277740d41da72bb7f1bc8e8f8429cdc5.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4d29181e14262e77701c580ba33a3b2a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ec5194a07e61dbad3e7b",
+    "url": "/static/css/main.66d6f871.chunk.css"
+  },
+  {
+    "revision": "078d8750ac3017505296",
+    "url": "/static/js/2.29e9a946.chunk.js"
+  },
+  {
+    "revision": "ec5194a07e61dbad3e7b",
+    "url": "/static/js/main.94b350f6.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
