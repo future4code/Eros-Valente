379610a1aaf261346bd869d55717ee39c2fd27b6
Eros Pinder Valente <eros.pv@gmail.com>
2020-06-02 12:53:33 -0300

    página de caastro labenusers

---
+++ semana7/labenusers/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import logo from './logo.svg';
+import axios from 'axios';
+import './App.css';
+
+class App extends React.Component {
+    state = {
+        cadastroNaTela: true
+    }
+  
+  
+
+
+
+    render() {
+        if(this.state.cadastroNaTela) {
+            
+        }
+
+    
+
+        return (
+            
+        )
+    
+    }
+
+  
+}
+
+export default App;
