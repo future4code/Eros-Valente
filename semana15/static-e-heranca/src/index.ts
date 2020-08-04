1ea4f77d2cdc37b83ab09abda733eaae7983ab2e
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-04 18:24:02 -0300

    exercicio 2

--- semana15/static-e-heranca/src/index.ts
+++ semana15/static-e-heranca/src/index.ts
@@ -1,9 +1,17 @@
 import { User } from "./User"
-
+import { Customer } from "./Customer"
 
 // 1 a) caso a nova instancia esteja tipada de acordo com a classe, ou sem tipo não daria pra acessar a propriedade password pois ela é privada, e além disso a classe User não possui nenhum método que retorne o password
 
 // 1 b) Uma vez apenas.
 
-const user1: User = new User(String(Date.now() * Math.random()), "email@email.com", "João", "senha_super_segura")
+// const user1: User = new User(String(Date.now() * Math.random()), "email@email.com", "João", "senha_super_segura")
+
+// 2 a) Mensagem impressa uma vez no terminal.
+
+// 2 b) Mensagem impressa uma vez no terminal. O super da classe Costumer convoca o construtor da classe pai User.
+
+// const user2: Customer = new Customer("8394030494", "user2@email.com", "sem nome", "passwor_nao_muito_seguro", "123456")
+
+
 
