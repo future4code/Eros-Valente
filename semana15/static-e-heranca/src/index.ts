e0119c769df5fd441874f08be529cc313ac17001
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-04 23:16:51 -0300

    exercicio 8

--- semana15/static-e-heranca/src/index.ts
+++ semana15/static-e-heranca/src/index.ts
@@ -1,7 +1,8 @@
 import { User } from "./User"
 import { Customer } from "./Customer"
 import { Employee } from "./Employee"
-
+import { Seller } from "./Seller"
+ 
 // 1 a) caso a nova instancia esteja tipada de acordo com a classe, ou sem tipo não daria pra acessar a propriedade password pois ela é privada, e além disso a classe User não possui nenhum método que retorne o password
 
 // 1 b) Uma vez apenas.
@@ -24,8 +25,15 @@ import { Employee } from "./Employee"
 // 6 a) Uma vez, pois o super convoca o construtor da classe pai User
 
 // 6 b) Todos os dados passados menos password que é um dado privado e sem método criado para acessá-lo
-const employee1: Employee = new Employee("10", "joão@email.com", "João", "0000000", new Date("01/01/1980"), 2000)
+// const employee1: Employee = new Employee("10", "joão@email.com", "João", "0000000", new Date("01/01/1980"), 2000)
 
 // console.log(`Exercicio 6 - b) \nid: ${employee1.getId()} \nEmail: ${employee1.getEmail()} \nName: ${employee1.getName()} \nAdmission date: ${employee1.getAdmissionDate()} \nBase salary: ${employee1.getBaseSalary()}`)
 
-console.log("Exercício 7 \nSalário total:", employee1.calculateTotalSalary().toFixed(2))
+// 7)
+//console.log("Exercício 7 \nSalário total:", employee1.calculateTotalSalary().toFixed(2))
+
+// 8 a)
+
+const seller1: Seller = new Seller("123", "seller1@email.com", "Teddy", "0987", new Date("01/02/2000"), 1500)
+
+console.log(`Exercício 8 - b) \nid: ${seller1.getId()} \nEmail: ${seller1.getEmail()} \nName: ${seller1.getName()} \nAdmission date: ${seller1.getAdmissionDate()} \nBase salary: ${seller1.getBaseSalary()}`)
