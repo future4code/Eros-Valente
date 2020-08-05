70a93acf1c28ba84f59d84389fde5065d0de12ad
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-05 12:32:56 -0300

    exercicio 2

--- semana15/polimorfismo/src/index.ts
+++ semana15/polimorfismo/src/index.ts
@@ -1,4 +1,5 @@
 import { Client } from "./Client"
+import { Place } from "./Place"
 
 const client: Client = {
     name: "José",
@@ -9,3 +10,7 @@ const client: Client = {
 }
 
 // 1 a) Todas as propriedades podem ser acessadas e impressas pois interfaces não possuem encapsulamento, tudo é publico.
+
+// 2 a) O typescript retorna o erro "Cannot create an instance of an abstract class." 
+// 2 b) As classes filhas da classe abstrata devem implementar os atributos e metodos da classe abstrata.
+
