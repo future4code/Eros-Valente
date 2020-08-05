1709cd6d701173ebfb84a15bf8e874238d7e7ddb
Eros Pinder Valente <eros.pv@gmail.com>
2020-08-05 13:30:00 -0300

    exercicio 3

--- semana15/polimorfismo/src/index.ts
+++ semana15/polimorfismo/src/index.ts
@@ -1,5 +1,8 @@
 import { Client } from "./Client"
 import { Place } from "./Place"
+import { Residence } from "./Residence"
+import { Commerce } from "./Commerce"
+import { Industry } from "./Industry"
 
 const client: Client = {
     name: "José",
@@ -12,5 +15,18 @@ const client: Client = {
 // 1 a) Todas as propriedades podem ser acessadas e impressas pois interfaces não possuem encapsulamento, tudo é publico.
 
 // 2 a) O typescript retorna o erro "Cannot create an instance of an abstract class." 
-// 2 b) As classes filhas da classe abstrata devem implementar os atributos e metodos da classe abstrata.
+// 2 b) As classes filhas da classe abstrata devem implementar os atributos e metodos da classe abstrata. Deve-se então criar uma instancia dessa classe filha
 
+// 3 )
+
+const home: Residence = new Residence(4, "09510-021")
+const store: Commerce = new Commerce(2, "08717-000")
+const factory: Industry = new Industry(15, "98765-010")
+
+console.log("Cep home: ", home.getCep())
+console.log("Cep store: ", store.getCep())
+console.log("Cep factory: ", factory.getCep())
+
+console.log("nº de moradores de home ", home.getResidentsQuantity())
+console.log("nº de moradores de home ", store.getFloorsQuantity() )
+console.log("nº de moradores de home ", factory.getMachinesQuantity() )
