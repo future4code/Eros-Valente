022b920aea67a6479b7f7d1a95f508fd399110fd
Eros Pinder Valente <eros.pv@gmail.com>
2020-09-01 15:55:31 -0300

    ex3

--- semana19/testes-unitarios-e-mocks/src/ex1.ts
+++ semana19/testes-unitarios-e-mocks/src/ex1.ts
@@ -1,8 +1,8 @@
 export interface Character {
     name: string,
-    life?: number,
-    strength?: number,
-    defense?: number 
+    life: number | undefined,
+    strength: number | undefined,
+    defense: number | undefined 
 }
 
 export function validateCharacter(input: Character): boolean {
